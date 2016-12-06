38df0e81dfea99769ec41aebc7d24bc7e32a9159
AlexandreClose <alexandre.close@gmail.com>
2016-12-06 19:42:47 +0100

    change SQL favorites table name

--- src/sql/plugins/favorites/plugin/create_db_favorites.sql
+++ src/sql/plugins/favorites/plugin/create_db_favorites.sql
@@ -3,8 +3,8 @@
 -- Structure for table favorites_favorite
 --
 
-DROP TABLE IF EXISTS favorites_favorite;
-CREATE TABLE favorites_favorite (
+DROP TABLE IF EXISTS mydashboard_favorites_favorite;
+CREATE TABLE mydashboard_favorites_favorite (
 id_favorite int(6) NOT NULL,
 label varchar(255) default '' NOT NULL,
 url varchar(255) default '' NOT NULL,
